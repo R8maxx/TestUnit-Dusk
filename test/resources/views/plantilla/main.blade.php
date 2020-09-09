199b7113739fbb44fa53445af92be52e209ae258
RM <kururygiroro@gmail.com>
2020-09-09 10:35:25 +0200

    Windows

--- test/resources/views/plantilla/main.blade.php
+++ test/resources/views/plantilla/main.blade.php
@@ -4,21 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-UA-Compatible" content="ie=edge">
-        <meta name="csrf-token" content="{{csrf_token()}}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>RM - @yield('title')</title>
 
-        <script src="{{assert('js/app.js')}}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
-        <link rel="icon" type="image/png" href="{{assert('img/logo.png')}}"/>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
+        <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}"/>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm site-header sticky-top">
             <!-- .container nos permite centrar el contenido de nuestro menu, esta clase es opcional y podemos encerrar el menu <nav> o incluir el contenedor dentro del <nav> -->
             <div class="container">
                 <!-- Nos sirve para agregar un logotipo al menu -->
-                <a href="#" class="navbar-brand">Menu</a>
+                <img src="{{asset('img/logo.png')}}" alt="Imagen de un logo" class="navbar-brand" width="4%" height="auto">
 
                 <!-- Nos permite usar el componente collapse para dispositivos moviles -->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Menu de Navegacion">
@@ -53,5 +53,59 @@
             </div>
         </nav>
 
+        <main>
+            @section('main')
+
+            @show
+        </main>
+
+        <footer class="py-5 bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md">
+                        <img src="{{asset('img/logo.png')}}" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></img>
+                        <small class="d-block mb-3 text-muted">&copy; 2017-2020</small>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5 class="text-white">Features</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Cool stuff</a></li>
+                            <li><a class="text-muted" href="#">Random feature</a></li>
+                            <li><a class="text-muted" href="#">Team feature</a></li>
+                            <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                            <li><a class="text-muted" href="#">Another one</a></li>
+                            <li><a class="text-muted" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5 class="text-white">Resources</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Resource</a></li>
+                            <li><a class="text-muted" href="#">Resource name</a></li>
+                            <li><a class="text-muted" href="#">Another resource</a></li>
+                            <li><a class="text-muted" href="#">Final resource</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5 class="text-white">Resources</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Business</a></li>
+                            <li><a class="text-muted" href="#">Education</a></li>
+                            <li><a class="text-muted" href="#">Government</a></li>
+                            <li><a class="text-muted" href="#">Gaming</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5 class="text-white">About</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Team</a></li>
+                            <li><a class="text-muted" href="#">Locations</a></li>
+                            <li><a class="text-muted" href="#">Privacy</a></li>
+                            <li><a class="text-muted" href="#">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
